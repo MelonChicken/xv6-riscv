443938c0094e53b50d49c6ac17ef9f30d08bcd8e
JUNU <m.l02n23@gmail.com>
2026-04-09 13:46:14 +0900

    feat (proc): todo comment

--- kernel/proc.c
+++ kernel/proc.c
@@ -540,7 +540,9 @@ scheduler(void)
     int found = 0;
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
+      // TODO 01: Lag determine
       if(p->state == RUNNABLE) {
+        // TODO: vdeadline comparison
         // Switch to chosen process.  It is the process's job
         // to release its lock and then reacquire it
         // before jumping back to us.
